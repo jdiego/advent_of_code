206fabe26c2b82fde4ca4c34650a10e61ffe2895
Diego Saraiva <diegosaraiva@gmail.com>
2022-12-17 17:42:30 -0300

    first day

---
+++ 2022/day1/main.cpp
@@ -0,0 +1,87 @@
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <string_view>
+#include <ranges>
+#include <numeric>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+auto max_calories(const std::vector<std::string>& data, std::string delimiter){
+    auto by_elf = data | 
+        // 1. split the input by-elf
+        std::views::lazy_split(delimiter) |
+        // 2. sum up the numbers, so we have a total for each elf
+        std::views::transform([](const auto& elf) -> std::uint64_t {
+            // 3. convet each calorie string into a number
+            auto to_number = [](const auto& in) { return std::stoull(in); };
+            auto rng = elf | std::views::transform(to_number) | std::views::common; 
+            return std::accumulate(rng.begin(), rng.end(), 0);
+        });
+    return by_elf;
+}
+
+auto top_one(const std::vector<std::string>& data, std::string delimiter = std::string{}){
+    auto by_elf = max_calories(data, delimiter);
+    // pick the elf with the highest total
+    auto it = std::ranges::max_element(by_elf);
+    return *it;
+}
+
+auto top_three(const std::vector<std::string>& data, std::string delimiter = std::string{}) {
+    auto by_elf = max_calories(data, delimiter);
+    // sort the elfs by calories
+    std::vector<uint64_t> top_elfs(3);
+    std::ranges::partial_sort_copy(by_elf, top_elfs, std::greater<>{});
+    // pick the top three elf with the most calories
+    return std::accumulate(top_elfs.begin(), top_elfs.end(), 0);
+
+}
+
+int parse_and_run(std::string_view path) {
+    std::vector<std::string> data;
+    std::fstream file(path.data());
+    if (!file.is_open()) {
+        std::cerr << "Failed to open " << std::quoted(path) << "\n";
+        return 1;
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        data.push_back(line);
+    }
+    std::cout << "The elf with the maximum number of calorie is carrying " 
+            << top_one(data) 
+            << " calories worth of food.\n";
+    std::cout << "The top 3 elfs are carrying " 
+            << top_three(data) 
+            << " calories worth of food.\n";
+
+    return 0;
+}
+
+int test_sample_input() {
+    std::vector<std::string> test_input{
+        "1000", "2000", "3000", " ",
+        "4000", " ",
+        "5000", "6000", " ",
+        "7000", "8000", "9000", " ",
+        "10000"
+    };
+    assert(top_one(test_input) == 24000);
+    return 0;
+}
+
+int main(int argc, char* argv[]) 
+{
+    if (argc == 1) {
+        return test_sample_input();
+    } 
+    else if (argc == 2) {
+        return parse_and_run(argv[1]);
+    }
+    
+    return 0; 
+}
---
+++ Utils/string_operations.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+#include <string>
+
+std::vector<std::string> split_string(std::string str, std::string delimiter){
+    
+}
